454c43e1d7305b1f26180fe831cb060da0849456
CarlosEscat <carlosescat@gmail.com>
2020-10-13 13:56:16 +0200

    Hiding key

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/stream", async (request, response) => {
   stream.init(request, response);
 });
 
-var subscriptionKey = "664f165f877f47c0bca5e0284e210922";
+var subscriptionKey = "INSERT_KEY_HERE";
 var endpoint = "https://api.cognitive.microsofttranslator.com";
 
 app.post("/message", async (request, response) => {
